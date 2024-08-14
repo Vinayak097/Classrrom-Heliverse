0c4a8d2e972fab8071b2b87cf98ebf5df1181b2e
unknown <vinayak20injamure@gmail.com>
2024-08-14 22:08:03 +0530

    m

--- backend/models/Classroom.js
+++ backend/models/Classroom.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
 const classroomSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true , unique: true},
   schedule: [{
     day: { type: String, required: true }, // e.g., "Monday"
     startTime: { type: String, required: true }, // e.g., "12:00 PM"
     endTime: { type: String, required: true } // e.g., "06:00 PM"
   }],
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  
 });
 
 const Classroom = mongoose.model('Classroom', classroomSchema);
